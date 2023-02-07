1fa23adf7ac7d66ee6802af01411ce469eb21642
Nick Maslov <kategorychnost@gmail.com>
2023-02-06 20:39:46 -0500

    setting up structure of the project;added tostify messages

---
+++ src/router/App.js
@@ -0,0 +1,26 @@
+// import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+    const notify = () =>
+        toast('🦄 Wow so easy!', {
+            position: 'top-right',
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: 'light',
+        });
+
+    return (
+        <div>
+            <button onClick={notify}>Notify!</button>
+            <ToastContainer />
+        </div>
+    );
+}
+
+export default App;
